e62d92e7d845b0f54e5f10837e1547d0a8575a5b
Marine Lancien (Hexagone IT) <marine.lancien@hexagone-it.fr>
2025-06-19 11:21:36 +0200

    Ajout des tests sur la page d’accueil
     - suppression des tests par défaut

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Household;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): RedirectResponse|View
     {
         $household = Household::orderBy('id')->first();
+        
+        if (!$household) {
+            return redirect()->route('household.settings');
+        }
         
         return view('home', compact('household'));
     }
