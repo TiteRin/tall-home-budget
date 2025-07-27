c4436130e34bb598ba2f1f887ba2f9e70ea6b74c
Idle Rin <marine.lancien@protonmail.com>
2025-07-27 15:25:29 +0200

    fix(config): correction de la configuration du projet lors de son initialisation
    - problème d'accès à la base de données
    - problème de génération des assets avec Vite

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,9 @@ export default defineConfig({
         host: '0.0.0.0',
         port: Number(process.env.VITE_PORT) || 5175,
         strictPort: true,
+        hmr: {
+            host: process.env.VITE_HOST || `localhost`,
+            port: Number(process.env.VITE_PORT) || 5175
+        }
     }
 });
