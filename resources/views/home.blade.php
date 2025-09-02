a193916793aa37e066fe1fbcf4dc4de8831bafac
Marine Lancien <marine.lancien@protonmail.com>
2025-09-02 11:26:23 +0200

    feat(HomePage): integrate AccountsList component into home view
    - Created `AccountsList` Livewire component for rendering account-related information.
    - Added `accounts-list.blade.php` view for structuring the component's UI.
    - Replaced static accounts card with dynamic Livewire component in `home.blade.php`.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,7 @@
 
     <div class="flex flex-col gap-4">
         <div class="w-full flex flex-row gap-4">
-            <section class="card bg-base-100 shadow-xl grow">
-                <div class="card-body">
-                    <h2 class="card-title">
-                        Comptes
-                    </h2>
-                </div>
-            </section>
+            @livewire('home.accounts-list')
             <section class="card bg-base-100 shadow-xl grow">
                 <div class="card-body">
                     <h2 class="card-title">
---
+++ resources/views/livewire/home/accounts-list.blade.php
@@ -0,0 +1,7 @@
+<section class="card bg-base-100 shadow-xl grow">
+    <div class="card-body">
+        <h2 class="card-title">
+            Comptes
+        </h2>
+    </div>
+</section>
