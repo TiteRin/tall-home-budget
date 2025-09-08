881432da34555e144dd1b1bb13ca98259ebe6177
Marine Lancien <marine.lancien@protonmail.com>
2025-09-08 10:00:07 +0200

    feat(Bill): extend BillsCollection functionalities and add summarization methods
    - Added `getTotal`, `getTotalFor`, and `getTotalForMember` methods for summarizing bill amounts.
    - Introduced `add` method to safely add a bill to the collection, preventing duplicates.
    - Expanded `BillsCollectionTest` to cover new methods and edge case validations.

--- app/Services/Bill/BillsCollection.php
+++ app/Services/Bill/BillsCollection.php
@@ -2,6 +2,11 @@
 
 namespace App\Services\Bill;
 
+use App\Domains\ValueObjects\Amount;
+use App\Enums\DistributionMethod;
+use App\Models\Bill;
+use App\Models\Member;
+use Exception;
 use Illuminate\Support\Collection;
 
 class BillsCollection
@@ -16,5 +21,35 @@ class BillsCollection
     public function toArray(): array
     {
         return $this->bills->toArray();
+    }
+
+    public function getTotal(): Amount
+    {
+        if ($this->bills->isEmpty()) {
+            return new Amount(0);
+        }
+
+        return new Amount($this->bills->sum(fn($bill) => $bill->amount->value()));
+    }
+
+    public function getTotalFor(DistributionMethod $distributionMethod): Amount
+    {
+        return (new BillsCollection($this->bills->filter(fn($bill) => $bill->distribution_method === $distributionMethod)))->getTotal();
+    }
+
+    public function getTotalForMember(?Member $member = null): Amount
+    {
+        return (new BillsCollection($this->bills->filter(fn($bill) => $bill->member_id === $member?->id)))->getTotal();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function add(Bill $bill): void
+    {
+        if ($this->bills->contains('id', $bill->id)) {
+            throw new Exception('Bill already exists');
+        }
+        $this->bills->push($bill);
     }
 }
--- tests/Unit/Services/Bill/BillsCollectionTest.php
+++ tests/Unit/Services/Bill/BillsCollectionTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit\Services\Bill;
 
+use App\Domains\ValueObjects\Amount;
+use App\Enums\DistributionMethod;
 use App\Services\Bill\BillsCollection;
+use Exception;
 
 beforeEach(function () {
     $this->household = bill_factory()->household(['has_joint_account' => true]);
@@ -27,4 +30,61 @@ test("should initialize with a collection of bills", function () {
     $collection = new BillsCollection($bills);
 
     expect($collection->toArray())->toHaveCount(3);
+});
+
+test("should add a bill to the collection", function () {
+    $bills = collect(bill_factory()->bills(3, [], $this->memberAlice, $this->household));
+    $collection = new BillsCollection($bills);
+
+    $collection->add(bill_factory()->bill([], $this->memberBob, $this->household));
+    expect($collection->toArray())->toHaveCount(4);
+});
+
+test("should not add a bill to the collection if it already exists", function () {
+    $bills = collect(bill_factory()->bills(3, [], $this->memberAlice, $this->household));
+    $collection = new BillsCollection($bills);
+
+    $collection->add($bills->first());
+})->throws(Exception::class, "Bill already exists");
+
+describe("Summarization", function () {
+
+    test("should obtain the total amount of the collection", function () {
+        $collection = new BillsCollection(
+            [
+                bill_factory()->bill(['name' => 'Internet', 'amount' => 3000], $this->memberAlice, $this->household),
+                bill_factory()->bill(['name' => 'Loyer', 'amount' => 67000], null, $this->household),
+                bill_factory()->bill(['name' => 'Électricité', 'amount' => 10000], $this->memberBob, $this->household),
+            ]
+        );
+
+        expect($collection->getTotal())->toEqual(new Amount(67000 + 3000 + 10000));
+    });
+
+    test("should obtain the total amount of the collection for a distribution method", function () {
+        $collection = new BillsCollection(
+            [
+                bill_factory()->bill(['name' => 'Internet', 'amount' => 3000, 'distribution_method' => DistributionMethod::PRORATA], $this->memberAlice, $this->household),
+                bill_factory()->bill(['name' => 'Loyer', 'amount' => 67000, 'distribution_method' => DistributionMethod::EQUAL], null, $this->household),
+                bill_factory()->bill(['name' => 'Électricité', 'amount' => 10000, 'distribution_method' => DistributionMethod::PRORATA], $this->memberBob, $this->household),
+            ]
+        );
+
+        expect($collection->getTotalFor(DistributionMethod::PRORATA))->toEqual(new Amount(3000 + 10000))
+            ->and($collection->getTotalFor(DistributionMethod::EQUAL))->toEqual(new Amount(67000));
+    });
+
+    test("should obtain the total for a member", function () {
+        $collection = new BillsCollection(
+            [
+                bill_factory()->bill(['name' => 'Internet', 'amount' => 3000, 'distribution_method' => DistributionMethod::PRORATA], $this->memberAlice, $this->household),
+                bill_factory()->bill(['name' => 'Loyer', 'amount' => 67000, 'distribution_method' => DistributionMethod::EQUAL, 'member_id' => null], null, $this->household),
+                bill_factory()->bill(['name' => 'Électricité', 'amount' => 10000, 'distribution_method' => DistributionMethod::PRORATA], $this->memberBob, $this->household),
+            ]
+        );
+
+        expect($collection->getTotalForMember($this->memberAlice))->toEqual(new Amount(3000))
+            ->and($collection->getTotalForMember($this->memberBob))->toEqual(new Amount(10000))
+            ->and($collection->getTotalForMember(null))->toEqual(new Amount(67000));
+    });
 });
