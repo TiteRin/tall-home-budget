3c52ab1402094789fb755814f7d9d1ca9f3af610
Marine Lancien <marine.lancien@protonmail.com>
2025-11-12 09:58:22 +0100

    chore: add `member_id` column to `users` table
    - Created migration to add `member_id` column with foreign key constraints and set to cascade on delete.
    - Updated `UserSchemaTest` to include tests for `member_id` column existence, nullability, uniqueness, and foreign key relationship.
    - Ensured `member_id` is unique when not null via unit tests.

--- tests/Unit/UserSchemaTest.php
+++ tests/Unit/UserSchemaTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\Models\Member;
+use App\Models\User;
+use Exception;
 use Illuminate\Support\Facades\Schema;
 
 describe('UserSchema', function () {
@@ -11,5 +14,25 @@ describe('UserSchema', function () {
 
     test('users table has email column', function () {
         expect(Schema::hasColumn('users', 'email'))->toBeTrue();
+    });
+
+    test('users table has member_id column', function () {
+        expect(Schema::hasColumn('users', 'member_id'))->toBeTrue();
+    });
+
+    test('member_id is nullable', function () {
+        $user = User::factory()->create(['member_id' => null]);
+        expect($user->member_id)->toBeNull();
+    });
+
+    test('member_id is unique when not null', function () {
+        $memberJohn = Member::factory()->create(['first_name' => 'John']);
+
+        $userJohn = User::factory()->create(['member_id' => $memberJohn->id]);
+        expect($userJohn->member_id)->toBe($memberJohn->id)
+            ->and(
+                fn() => User::factory()->create(['member_id' => $memberJohn->id])
+            )->toThrow(Exception::class);
+
     });
 });
