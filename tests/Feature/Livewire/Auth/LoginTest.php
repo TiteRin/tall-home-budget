dc52b1e0f0b4f4adae088fadec0f4e6140838559
Marine Lancien <marine.lancien@protonmail.com>
2026-01-08 09:54:02 +0100

    chore: add `LoginTest` with Livewire implementation
    - Added tests for accessing the login page and verifying email/password fields visibility.
    - Implemented tests for form submission with valid and invalid credentials.
    - Verified redirection to the home page and authentication status for successful logins.

--- tests/Feature/Livewire/Auth/LoginTest.php
+++ tests/Feature/Livewire/Auth/LoginTest.php
@@ -1,7 +1,49 @@
 <?php
 
+use App\Livewire\Auth\Login;
+use App\Models\User;
+use Livewire\Livewire;
+
 test('can access Login Page', function () {
     $response = $this->get('/login');
 
     $response->assertStatus(200);
+});
+
+test('can see email and password fields', function () {
+    Livewire::test(Login::class)
+        ->assertSeeHtml('name="email"')
+        ->assertSeeHtml('type="password"');
+});
+
+test('can fill fields and submit form', function () {
+    $user = User::factory()->create();
+
+    Livewire::test(Login::class)
+        ->set('email', $user->email)
+        ->set('password', 'password')
+        ->call('authenticate')
+        ->assertHasNoErrors();
+});
+
+test('when using wrong credentials, should display an error message', function () {
+    Livewire::test(Login::class)
+        ->set('email', 'wrong@example.com')
+        ->set('password', 'wrongpassword')
+        ->call('authenticate')
+        ->assertHasErrors(['email']);
+});
+
+test('when using correct credentials, should redirect to home page', function () {
+    $user = User::factory()->create([
+        'password' => bcrypt($password = 'i-love-laravel'),
+    ]);
+
+    Livewire::test(Login::class)
+        ->set('email', $user->email)
+        ->set('password', $password)
+        ->call('authenticate')
+        ->assertRedirect(route('home'));
+
+    $this->assertAuthenticatedAs($user);
 });
