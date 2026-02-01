e09d947e629841611a8cd6edfe8eab5a3ded0b73
Marine Lancien <marine.lancien@protonmail.com>
2026-02-01 13:58:55 +0100

    feat[ExpenseTabs]: update expense tab list layout with tabbed interface
    - Replaced `<ul>` with a tabbed layout structure for improved UI/UX.
    - Implemented radio-based tabs for expense tab navigation.
    - Added "Nouvel onglet de dépense" as a default tab with placeholder content.
    - Adjusted styling with `bg-base-100` and `border-base-300` classes for consistency.

--- resources/views/livewire/expense-tabs/list.blade.php
+++ resources/views/livewire/expense-tabs/list.blade.php
@@ -1,5 +1,20 @@
-<ul>
+<div class="tabs tabs-lift tabs-xl" role="tablist">
+    <label class="tab">
+        <input type="radio" name="tabs-expense-tabs" checked/>
+        Nouvel onglet de dépense
+    </label>
+    <div class="tab-content bg-base-100 border-base-300 p-6">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut debitis eos et iste odit, officiis reiciendis?
+        Animi architecto ex fugit labore magnam optio quibusdam sequi! Atque hic provident quam soluta.
+    </div>
+
     @foreach ($expenseTabs as $tab)
-        <li>{{ $tab->name }}</li>
+        <label class="tab">
+            <input type="radio" name="tabs-expense-tabs"/>
+            {{ $tab->name }}
+        </label>
+        <div class="tab-content bg-base-100 border-base-300 p-6">
+
+        </div>
     @endforeach
-</ul>
+</div>
