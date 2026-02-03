51a8a8099fb167cec0665e49664a5b38367deb5a
Marine Lancien <marine.lancien@protonmail.com>
2026-02-03 16:33:32 +0100

    refactor[Home Layout]: improve responsiveness for home components
    - Updated `home.blade.php` to switch layout from column to row on medium screens.
    - Adjusted `movements-list` and `movements-list-empty` components to use full-width on small screens and half-width on medium screens.

--- resources/views/livewire/home/home.blade.php
+++ resources/views/livewire/home/home.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col gap-4">
-    <div class="w-full flex flex-row gap-4">
+    <div class="w-full flex flex-col md:flex-row gap-4 ">
         @livewire('home.accounts-list', ['members' => $members])
         @livewire('home.movements.movements-list', ['incomes' => $incomes], key('movements-list'))
     </div>
